5e0abfd07d1750af60ce9c592c931f9a4d22c5e0
Guilherme <guilhermemoura90@gmail.com>
2021-02-06 01:06:45 -0300

    Namespace

--- Authorizer/Program.cs
+++ Authorizer/Program.cs
@@ -11,24 +11,24 @@ namespace Authorizer
     {
         static void Main(string[] args)
         {
-            var textRead = new StringReader(
-                @"{""account"": {""active-card"": true, ""available-limit"": 100}}" +
-                Environment.NewLine +
-                @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 20, ""time"":""2019-02-13T09:59:00.000Z""}}" +
-                Environment.NewLine +
-                @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 1, ""time"":""2019-02-13T09:59:00.000Z""}}" +
-                Environment.NewLine +
-                @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 100, ""time"":""2019-02-13T09:59:00.000Z""}}" +
-                Environment.NewLine +
-                @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 2, ""time"":""2019-02-13T09:59:00.000Z""}}" +
-                Environment.NewLine +
-                @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 1, ""time"":""2019-02-13T10:00:00.000Z""}}");
+            //var textRead = new StringReader(
+            //    @"{""account"": {""active-card"": true, ""available-limit"": 100}}" +
+            //    Environment.NewLine +
+            //    @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 20, ""time"":""2019-02-13T09:59:00.000Z""}}" +
+            //    Environment.NewLine +
+            //    @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 1, ""time"":""2019-02-13T09:59:00.000Z""}}" +
+            //    Environment.NewLine +
+            //    @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 100, ""time"":""2019-02-13T09:59:00.000Z""}}" +
+            //    Environment.NewLine +
+            //    @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 2, ""time"":""2019-02-13T09:59:00.000Z""}}" +
+            //    Environment.NewLine +
+            //    @"{""transaction"": {""merchant"": ""Burger King"", ""amount"": 1, ""time"":""2019-02-13T10:00:00.000Z""}}");
 
 
             DependencyContainer.Initialize();
 
-            //var reader = Console.In;
-            var reader = textRead;
+            var reader = Console.In;
+            //var reader = textRead;
             var output = new CommandExecuter(reader).Execute();
 
             output.ForEachAsync(q =>
